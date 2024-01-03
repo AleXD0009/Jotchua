3ac93838e77b930bdd9a3ab783f4bb8e92ac3c35
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-03 09:16:43 -0300

    Update maker-ytcomment.js

--- plugins/maker-ytcomment.js
+++ plugins/maker-ytcomment.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, text}) => {
     avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     comment: text,
     username: conn.getName(m.sender),
-  }), 'error.png', '*¡¡𝙶𝚁𝙰𝙲𝙸𝙰𝚂 𝙿𝙾𝚁 𝙲𝙾𝙼𝙴𝙽𝚃𝙰𝚁!!*', m);
+  }), 'error.png', '*¡¡aqui esta tu comentario xdd!!*', m);
 };
 handler.help = ['ytcomment <comment>'];
 handler.tags = ['maker'];

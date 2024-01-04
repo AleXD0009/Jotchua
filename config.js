0fc113e689d64c15ce2152abc32b060b1bb1a1aa
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-04 06:27:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ global.igfg = 'Jotchua-Bot By';
 global.wait = '*Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
-global.menuvid = fs.readFileSync('./media/src/Menuvid1.mp4');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
 global.imagen5 = fs.readFileSync('./src/+18.jpg');

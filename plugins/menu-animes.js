f50aaaf0af93a5bfd5f0462aaa7f1aefe3afdfbf
Ale XD <145930757+AleXD0009@users.noreply.github.com>
2024-01-04 00:19:17 -0300

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   try {
     const pp = menuvid;
-    // let vn = '.src/MenuAnime.mp4'
+//let vid = './media/src/menuvid.mp4''
     const d = new Date(new Date + 3600000);
     const locale = 'es';
     const week = d.toLocaleDateString(locale, {weekday: 'long'});
